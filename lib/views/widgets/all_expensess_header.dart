798d04432ba8403e3c690d505b1c0209e17d4dbe
nasr <naserallahataya@gmail.com>
2025-04-10 12:03:44 +0300

    create expensess

---
+++ lib/views/widgets/all_expensess_header.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ALlExpensessHeader extends StatelessWidget {
+  const ALlExpensessHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/views/widgets/dashboard_desktop_layout.dart
+++ lib/views/widgets/dashboard_desktop_layout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/views/widgets/all_expensess.dart';
 import 'package:responsive_dashboard/views/widgets/custom_drawer.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
@@ -6,6 +7,15 @@ class DashboardDesktopLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(children: [Expanded(child: CustomDrawer())]);
+    return const Row(
+      children: [
+        Expanded(child: CustomDrawer()),
+        SizedBox(height: 32),
+        Expanded(
+          flex: 2,
+          child: Column(children: [Expanded(child: AllExpensess())]),
+        ),
+      ],
+    );
   }
 }
